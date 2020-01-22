886e5e0c81d7ffcde6a82431d29ad9c86cff0f0d
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-01-22 16:03:07 -0500

    Update createtables.sql

--- createtables.sql
+++ createtables.sql
@@ -22,3 +22,12 @@ CREATE TABLE bme680sensors ( uuid STRING, `end` STRING, systemtime STRING, runti
 
 CREATE TABLE bme280sensors ( uuid STRING, `end` STRING, systemtime STRING, runtime STRING, cpu DOUBLE, id STRING,  te STRING,  host STRING, macaddress STRING, diskusage STRING, memory DOUBLE, ipaddress STRING,  host_name STRING, bme280_altitude STRING, bme280_tempf STRING, max30105timestamp STRING,  max30105_detected STRING, max30105_delta STRING, max30105_temp STRING, bme280_tempc STRING, max30105_mean STRING, max30105_value STRING, bme280_altitude_feet STRING, bme280_pressure STRING,  starttime STRING, cputemp DOUBLE, imgnamep STRING, imgname STRING, PRIMARY KEY (uuid, `end`) ) PARTITION BY HASH PARTITIONS 4 STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
+CREATE TABLE movidius ( uuid STRING,  endtime STRING, systemtime STRING, runtime STRING, cputemp STRING, id STRING, te STRING, 
+host STRING,starttime STRING,image_filename STRING, 
+macaddress STRING, diskfree STRING, memory STRING, ipaddress STRING, host_name STRING,
+PRIMARY KEY (uuid, endtime)
+)
+PARTITION BY HASH PARTITIONS 4
+STORED AS KUDU
+TBLPROPERTIES ('kudu.num_tablet_replicas' = '1')
+;
