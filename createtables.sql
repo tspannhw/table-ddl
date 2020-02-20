ebe4748b11c72387c417bba707f3d497f9295aa6
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-02-20 13:48:18 -0500

    Update createtables.sql

--- createtables.sql
+++ createtables.sql
@@ -53,4 +53,25 @@ PRIMARY KEY (uuid, systemtime)
 )
 PARTITION BY HASH PARTITIONS 4
 STORED AS KUDU
-TBLPROPERTIES ('kudu.num_tablet_replicas' = '1')
+TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
+
+CREATE TABLE coral 
+( id STRING, systemtime STRING, humidity STRING,
+pressure STRING,
+ambient_light STRING, 
+temperature STRING,
+tempf STRING,
+runtime STRING, 
+score_1 STRING,
+message STRING, 
+cputemp DOUBLE,
+starttime STRING,
+cpu DOUBLE,
+te STRING,  host STRING, macaddress STRING, diskusage STRING, 
+memory DOUBLE,
+ip STRING, host_name STRING, 
+label_1 STRING, 
+PRIMARY KEY (id, systemtime) ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1'); 
